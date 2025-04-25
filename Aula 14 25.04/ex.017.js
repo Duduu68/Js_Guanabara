c868d1882a3988c72e0b53ca3f5b9c12c8e85fe4
EduardoLucena <eduardolucena02@gmail.com>
2025-04-25 08:48:53 -0300

    Usando e aprendendo a usar o (do)

--- Aula 14 25.04/ex.017.js
+++ Aula 14 25.04/ex.017.js
@@ -1,6 +1,14 @@
+// var c = 1
+
+// while(c <= 10) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+
 var c = 1
 
-while(c < 6) {
-    console.log('Tudo Bem ?')
+do {
+    console.log(`Passo ${c}`)
     c++
-}
+}   while (c <= 6)
