fd30f623b36116f1ba1eba544d444d5fc5f79b96
EduardoLucena <eduardolucena02@gmail.com>
2025-04-25 08:59:51 -0300

    Aprendendo a usar o for em js

---
+++ Aula 15 25.04/ex.018.js
@@ -0,0 +1,14 @@
+// for tem a inicialização primeiro, depois o teste e o incremento 
+//exemplo: for(inicialização;teste;incremento)
+
+// var c = 1 inicialização
+
+// while(c<=10) teste{
+    //console.log(c)
+//     c++ incremento
+// }
+
+
+for(var c=1;c<=10;c++) {
+    console.log(c)
+}
